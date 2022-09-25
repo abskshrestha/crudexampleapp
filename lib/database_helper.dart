da2e7b28bb8562badaf777dfdc72c0cec027e1c5
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-09-25 23:56:03 +0545

    sqflite and path provider

---
+++ lib/database_helper.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "MyDatabase.db";
+  static final  _databaseVersion = 1;
+  static final _tableName = "myTable";
+
+  static final columnId = '_id';
+  static final columnName = 'name';
+
+  DatabaseHelper._privateconstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateconstructor();
+
+  static late final Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initiateDatabase();
+    return _database;
+  }
+
+  _initiateDatabase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+
+    String path = join(directory.path, _databaseName);
+
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    db.query(''' // treat all the lines as single string
+      Create table $_tableName (
+        $columnId INTEGER PRIMARY KEY, 
+        $columnName TEXT NOT NULL)
+      ''');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(_tableName, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll() async {
+    Database db = await instance.database;
+    return await db.query(_tableName);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    int id = row[columnId];
+    return await db
+        .update(_tableName, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return await db.delete(_tableName, where: '$columnId = ?', whereArgs: [id]);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crud Example App'),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
